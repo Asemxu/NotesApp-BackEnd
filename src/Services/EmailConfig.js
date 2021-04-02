588eb4a8d4929dcddc4c11a90c58fa69bc48b552
Asemxu <hijo72@hotmail.com>
2021-04-02 02:29:44 -0500

    Update Server

--- src/Services/EmailConfig.js
+++ src/Services/EmailConfig.js
@@ -1,11 +1,14 @@
 const transporter =  (nodemailer) => { 
     return nodemailer.createTransport({
         service:process.env['SERVICE'],
+        host:'smtp.gmail.com',
+        port: 587,
+        secure:true,
         auth: {
             user: process.env['EMAIL'],
             pass: process.env['PASS']
         },
-      
+       
     })
     
 }
