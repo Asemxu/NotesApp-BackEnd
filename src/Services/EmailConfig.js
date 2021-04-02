b5ea66fd2d8f20687e82a9fd5c5f73e9297e7a5e
Asemxu <hijo72@hotmail.com>
2021-04-02 03:06:00 -0500

    Server

--- src/Services/EmailConfig.js
+++ src/Services/EmailConfig.js
@@ -2,6 +2,7 @@ const transporter =  (nodemailer) => {
     return nodemailer.createTransport({
         service:process.env['SERVICE'],
         port:465,
+        secure:true,
         auth: {
             user: process.env['EMAIL'],
             pass: process.env['PASS']
