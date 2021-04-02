c296862a4bae48ba74823c4dc3e2c545daf2335e
Asemxu <hijo72@hotmail.com>
2021-04-02 02:15:05 -0500

    Update Server

--- src/Services/EmailConfig.js
+++ src/Services/EmailConfig.js
@@ -4,7 +4,11 @@ const transporter =  (nodemailer) => {
         auth: {
             user: process.env['EMAIL'],
             pass: process.env['PASS']
-        } 
+        },
+        tls: {
+            // do not fail on invalid certs
+            rejectUnauthorized: false
+        },
     })
     
 }
